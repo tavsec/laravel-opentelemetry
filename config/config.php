f0ee309c37cca67b844bd7713adf155bcf973dbb
Timotej Avsec <avsectimotej1@gmail.com>
2023-05-13 14:35:11 +0200

    Add body and header masking

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 return [
     "url" => env("OPENTELEMETRY_URL", "http://localhost:9411"),
-    "attribute_length_limit" => 4095
+    "attribute_length_limit" => 4095,
+    "masked_attributes" => ["password", "auth_token", "authorization", "laravel_session"],
+    "masked_attributes_shown_characters" => 3,
 ];
