937e5f83ba4ac4334355321bb9d0be0e9a839084
Timotej Avsec <avsectimotej1@gmail.com>
2023-05-25 21:27:10 +0200

    Add message sent listener

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -10,8 +10,10 @@ use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Http\Client\Events\RequestSending;
 use Illuminate\Http\Client\Events\ResponseReceived;
+use Illuminate\Mail\Events\MessageSent;
 use Tavsec\LaravelOpentelemetry\Listeners\CacheHitListener;
 use Tavsec\LaravelOpentelemetry\Listeners\CacheMissedListener;
+use Tavsec\LaravelOpentelemetry\Listeners\MessageSentListener;
 use Tavsec\LaravelOpentelemetry\Listeners\QueryExecutedListener;
 use Tavsec\LaravelOpentelemetry\Listeners\RequestSendingListener;
 use Tavsec\LaravelOpentelemetry\Listeners\ResponseReceivedListener;
@@ -24,7 +26,8 @@ class EventServiceProvider extends ServiceProvider
         CacheHit::class => [CacheHitListener::class],
         CacheMissed::class => [CacheMissedListener::class],
         ResponseReceived::class => [ResponseReceivedListener::class],
-        ScheduledTaskFinished::class => [ScheduledTaskFinishedListener::class]
+        ScheduledTaskFinished::class => [ScheduledTaskFinishedListener::class],
+        MessageSent::class => [MessageSentListener::class]
     ];
 
     public function boot()
